665ab29f08e4248152253372570642a12b6fbf8a
Yutong Yi <YiAndrew2001@gmail.com>
2022-03-05 04:29:38 -0800

    Added respawn flag animation. Refined respawn code.

--- TeamDemon_Unity/Assets/Scripts/PlayerRespawnBorder.cs
+++ TeamDemon_Unity/Assets/Scripts/PlayerRespawnBorder.cs
@@ -6,17 +6,20 @@ public class PlayerRespawnBorder : MonoBehaviour
 {
     [SerializeField] private Transform player;
     [SerializeField] private Transform respawnPoint;
+    private Vector3 respawnOffset = new Vector3(0f, 3f, 0f);
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            RespawnPlayer();
+            RespawnPlayer(collision);
         }
     }
 
-    private void RespawnPlayer()
+    private void RespawnPlayer(Collider2D collision)
     {
-        player.transform.position = respawnPoint.transform.position;
+        player.transform.position = respawnPoint.transform.position + respawnOffset;
+        Rigidbody2D rb = collision.GetComponent<Rigidbody2D>();
+        rb.velocity = Vector2.zero;
     }
 }
