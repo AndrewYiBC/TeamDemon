c1daf2ceb9585c101451c90ad00a70c8ba088232
Yutong Yi <YiAndrew2001@gmail.com>
2022-05-27 14:44:49 -0700

    Integrated pause menu system with main scene
    Updated pause menu components

--- TeamDemon_Unity/Assets/PauseMenu.cs
+++ TeamDemon_Unity/Assets/PauseMenu.cs
@@ -38,6 +38,6 @@ public class PauseMenu : MonoBehaviour
     public void QuitGame()
     {
         Debug.Log("Quitting Game");
-        Application.Quit();
+        // Application.Quit();
     }
 }
